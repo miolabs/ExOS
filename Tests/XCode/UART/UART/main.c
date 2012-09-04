bcbb54b2c204df03232e67848f331f51d8a70565
GodShadow <javier.segura@me.com>
2012-09-04 13:07:27 +0200

    Adding tests

---
+++ Tests/XCode/UART/UART/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  UART
+//
+//  Created by GodShadow on 04/09/12.
+//  Copyright (c) 2012 MIOLabs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
