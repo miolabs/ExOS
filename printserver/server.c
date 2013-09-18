2a5e9dee75e2fa481608226bf8996e70ff2349b6
Miguel Fides <miguelfides@gmail.com>
2013-09-18 19:52:30 +0200

    dual thread server

--- printserver/server.c
+++ printserver/server.c
@@ -5,25 +5,40 @@
 #include <stdio.h>
 
 #define SERVER_THREAD_STACK 1024
-static EXOS_THREAD _thread;
-static unsigned char _thread_stack[SERVER_THREAD_STACK];
+static EXOS_THREAD _thread0;
+static unsigned char _thread_stack0[SERVER_THREAD_STACK];
+static EXOS_THREAD _thread1;
+static unsigned char _thread_stack1[SERVER_THREAD_STACK];
 
 COMM_IO_ENTRY _comm;
 TCP_IO_ENTRY _socket;
 static unsigned char _buffer[1024] ;
 
-#define TCP_BUFFER_SIZE 8192 // tiny window for stress
-static unsigned char _rcv_buffer[TCP_BUFFER_SIZE]; 
-static unsigned char _snd_buffer[TCP_BUFFER_SIZE] __attribute__((section(".dma")));
+typedef struct
+{
+	unsigned char *RcvBuffer;
+	unsigned char *SndBuffer;
+	unsigned short Port;
+} _SERVER_CFG;
+
+#define TCP_BUFFER_SIZE 4096
+static unsigned char _rcv_buffer0[TCP_BUFFER_SIZE]; 
+static unsigned char _snd_buffer0[TCP_BUFFER_SIZE] __attribute__((section(".dma")));
+static _SERVER_CFG _cfg0 = { .RcvBuffer = _rcv_buffer0, .SndBuffer = _snd_buffer0, .Port = 9000 };
+static unsigned char _rcv_buffer1[TCP_BUFFER_SIZE]; 
+static unsigned char _snd_buffer1[TCP_BUFFER_SIZE] __attribute__((section(".dma")));
+static _SERVER_CFG _cfg1 = { .RcvBuffer = _rcv_buffer1, .SndBuffer = _snd_buffer1, .Port = 9001 };
 static void *_server(void *);
 
 void server_start()
 {
-	exos_thread_create(&_thread, 0, _thread_stack, SERVER_THREAD_STACK, NULL, _server, NULL); 
+	exos_thread_create(&_thread0, 0, _thread_stack0, SERVER_THREAD_STACK, NULL, _server, &_cfg0); 
+	exos_thread_create(&_thread1, 0, _thread_stack1, SERVER_THREAD_STACK, NULL, _server, &_cfg1); 
 }
 
 static void *_server(void *arg)
 {
+	_SERVER_CFG *cfg = (_SERVER_CFG *)arg;
 	int err;
 	int done;
 	int total;
@@ -31,12 +46,12 @@ static void *_server(void *arg)
 	while(1)
 	{
 		EXOS_IO_STREAM_BUFFERS buffers = (EXOS_IO_STREAM_BUFFERS) {
-			.RcvBuffer = _rcv_buffer, .RcvBufferSize = TCP_BUFFER_SIZE,
-			.SndBuffer = _snd_buffer, .SndBufferSize = TCP_BUFFER_SIZE };
+			.RcvBuffer = cfg->RcvBuffer, .RcvBufferSize = TCP_BUFFER_SIZE,
+			.SndBuffer = cfg->SndBuffer, .SndBufferSize = TCP_BUFFER_SIZE };
 
 		net_tcp_io_create(&_socket, EXOS_IOF_WAIT);
 
-		IP_PORT_ADDR local = (IP_PORT_ADDR) { .Address = IP_ADDR_ANY, .Port = 9100 };
+		IP_PORT_ADDR local = (IP_PORT_ADDR) { .Address = IP_ADDR_ANY, .Port = cfg->Port };
 		err = net_io_bind((NET_IO_ENTRY *)&_socket, &local); 
 		err = net_io_listen((NET_IO_ENTRY *)&_socket);
 
@@ -45,11 +60,11 @@ static void *_server(void *arg)
 		
 		exos_io_set_timeout((EXOS_IO_ENTRY *)&_socket, 2000); // NOTE: we didn't set timeout before because we don't want accept()  to timeout
 
-		EXOS_TREE_DEVICE *dev_node = (EXOS_TREE_DEVICE *)exos_tree_find_node(NULL, "dev/usbprint");
+		EXOS_TREE_DEVICE *dev_node = (EXOS_TREE_DEVICE *)exos_tree_find_path(NULL, "dev/usbprint");
 		if (dev_node == NULL)
-			dev_node = (EXOS_TREE_DEVICE *)exos_tree_find_node(NULL, "dev/usbftdi0");
+			dev_node = (EXOS_TREE_DEVICE *)exos_tree_find_path(NULL, "dev/usbftdi0");
 		if (dev_node == NULL)
-			dev_node = (EXOS_TREE_DEVICE *)exos_tree_find_node(NULL, "dev/comm0");
+			dev_node = (EXOS_TREE_DEVICE *)exos_tree_find_path(NULL, "dev/comm0");
 
 		if (dev_node != NULL)
 		{
