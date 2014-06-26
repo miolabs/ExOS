d7e3458ed89b8394f205e660bce7fec10295f35d
Miguel Fides <miguelfides@gmail.com>
2014-06-26 20:48:49 +0200

    adc hal changes and lpc17 support

--- support/lpc17/timer.c
+++ support/lpc17/timer.c
@@ -4,12 +4,13 @@
 #include "timer.h"
 #include "cpu.h"
 #include <support/cap_hal.h>
-#include <support/board_hal.h>
+#include <support/pwm_hal.h>
 
-static HAL_CAP_HANDLER _capture_handlers[4];
-static TIMER_MAT_HANDLER _match_handlers[4];
+static LPC_TIM_TypeDef *_modules[] = { LPC_TIM0, LPC_TIM1, LPC_TIM2, LPC_TIM3 };
+static HAL_CAP_HANDLER _cap_handlers[4];
+static HAL_PWM_HANDLER _mat_handlers[4];
 
-static LPC_TIM_TypeDef *_initialize(int module, unsigned long freq, TIMER_MAT_HANDLER callback)
+static LPC_TIM_TypeDef *_initialize(int module, unsigned long freq)
 {
 	LPC_TIM_TypeDef *timer;
     IRQn_Type irq;
@@ -68,36 +69,30 @@ static LPC_TIM_TypeDef *_initialize(int module, unsigned long freq, TIMER_MAT_HA
 	timer->CCR = 0;
 	timer->TC = 0;
 
-	_capture_handlers[module] = (HAL_CAP_HANDLER)0;
-	_match_handlers[module] = callback;
-
-	NVIC_EnableIRQ(irq);
+//	_capture_handlers[module] = (HAL_CAP_HANDLER)0;
+//	_match_handlers[module] = callback;
+//
+//	NVIC_EnableIRQ(irq);
 	return timer;
-}
-
-int timer_initialize(int module, unsigned long freq, TIMER_MAT_HANDLER callback)
-{
-	int mask = hal_board_init_pinmux(HAL_RESOURCE_MAT, module);
-	_initialize(module, freq, callback);
-	return mask;
 }
 
 int hal_cap_initialize(int module, unsigned long freq, HAL_CAP_MODE mode, HAL_CAP_HANDLER callback)
 {
-	int mask = hal_board_init_pinmux(HAL_RESOURCE_CAP, module);
-	LPC_TIM_TypeDef *timer = _initialize(module, freq, 0);
-	
+	LPC_TIM_TypeDef *timer = _initialize(module, freq);
 	if (timer)
 	{
-		_capture_handlers[module] = callback;
-	
-		TIMER_CAPM capm = CAPM_INT;
-		if (mode & HAL_CAP_RISING) capm |= CAPM_RISING;
-		if (mode & HAL_CAP_FALLING) capm |= CAPM_FALLING;
-	
-		TIMER_CCR *ccr = (TIMER_CCR *)&timer->CCR;
-		ccr->CR0 = capm;
-		ccr->CR1 = capm;
+		_cap_handlers[module] = callback;
+		timer->CCR = (callback ? TIMER_CCR_CAP0I : 0) |
+			((mode & HAL_CAP_RISING) ? TIMER_CCR_CAP0RE : 0) |
+            ((mode & HAL_CAP_FALLING) ? TIMER_CCR_CAP0FE : 0);
+
+		switch(module)
+		{
+			case 0: NVIC_EnableIRQ(TIMER0_IRQn);	break;
+			case 1: NVIC_EnableIRQ(TIMER1_IRQn);	break;
+			case 2: NVIC_EnableIRQ(TIMER2_IRQn);	break;
+			case 3: NVIC_EnableIRQ(TIMER3_IRQn);	break;
+		}
 
 		timer->TCR = 1;
 		return 1;
@@ -107,38 +102,31 @@ int hal_cap_initialize(int module, unsigned long freq, HAL_CAP_MODE mode, HAL_CA
 
 static void _isr(int module, LPC_TIM_TypeDef *timer)
 {
-//	TIMER_EVENT_HANDLER match_handler = _match_handlers[module];
-	if (timer->IR & TIMER_IR_MR0)
+	if (timer->IR & (TIMER_IR_CAP0 | TIMER_IR_CAP1))
 	{
-//		if (match_handler) match_handler(0, timer->MR[0], timer->TC);
-		timer->IR = TIMER_IR_MR0;
+		HAL_CAP_HANDLER handler = _cap_handlers[module];
+		if (handler)
+		{
+			if (timer->IR & TIMER_IR_CAP0)
+			{
+				handler(0, timer->CR0);
+				timer->IR = TIMER_IR_CAP0;
+			}
+			if (timer->IR & TIMER_IR_CAP1)
+			{
+				handler(1, timer->CR1);
+				timer->IR = TIMER_IR_CAP1;
+			}
+		}
 	}
-	if (timer->IR & TIMER_IR_MR1)
+
+	if (timer->IR & (TIMER_IR_MAT0|TIMER_IR_MAT1|TIMER_IR_MAT2|TIMER_IR_MAT3))
 	{
-//		if (match_handler) match_handler(1, timer->MR[1], timer->TC);
-		timer->IR = TIMER_IR_MR1;
-	}
-	if (timer->IR & TIMER_IR_MR2)
-	{
-//		if (match_handler) match_handler(2, timer->MR[2], timer->TC);
-		timer->IR = TIMER_IR_MR2;
-	}
-	if (timer->IR & TIMER_IR_MR3)
-	{
-//		if (match_handler) match_handler(3, timer->MR[3], timer->TC);
-		timer->IR = TIMER_IR_MR3;
-	}
-    
-	HAL_CAP_HANDLER capture_handler = _capture_handlers[module];
-	if (timer->IR & TIMER_IR_CR0)
-	{
-   		if (capture_handler) capture_handler(0, timer->CR0);
-		timer->IR = TIMER_IR_CR0;
-	}
-	if (timer->IR & TIMER_IR_CR1)
-	{
-   		if (capture_handler) capture_handler(1, timer->CR1);
-		timer->IR = TIMER_IR_CR1;
+		HAL_PWM_HANDLER handler = _mat_handlers[module];
+		if (handler)
+			handler(module);
+		// NOTE: We are only interested in one match channel irq for pwm
+		timer->IR = (TIMER_IR_MAT0|TIMER_IR_MAT1|TIMER_IR_MAT2|TIMER_IR_MAT3);
 	}
 }
 
@@ -162,3 +150,113 @@ void TIMER3_IRQHandler()
 	_isr(3, LPC_TIM3);
 }
 
+int timer_match_initialize(int module, int range, int rate, int channel_for_period)
+{
+	LPC_TIM_TypeDef *timer = _initialize(module, range * rate);
+	if (timer)
+	{
+		timer->MR0 = (channel_for_period == 0) ? range - 1 : range;
+		timer->MR1 = (channel_for_period == 1) ? range - 1 : range;
+		timer->MR2 = (channel_for_period == 2) ? range - 1 : range;
+		timer->MR3 = (channel_for_period == 3) ? range - 1 : range;
+
+		switch(channel_for_period)
+		{
+			case 0:
+				timer->MCR = TIMER_MCR_MR0R;
+				break;
+			case 1:
+				timer->MCR = TIMER_MCR_MR1R;
+				break;
+			case 2:
+				timer->MCR = TIMER_MCR_MR2R;
+				break;
+			case 3:
+				timer->MCR = TIMER_MCR_MR3R;
+				break;
+		}
+		timer->TCR = 1;
+		return 1;
+	}
+	return 0; 
+}
+
+void timer_match_set_handler(int module, int channel, HAL_PWM_HANDLER callback)
+{
+       	_mat_handlers[module] = callback;
+		LPC_TIM_TypeDef *timer = _modules[module];
+
+		timer->MCR &= ~(TIMER_MCR_MR0I | TIMER_MCR_MR1I | TIMER_MCR_MR2I | TIMER_MCR_MR3I);
+		switch(channel)
+		{
+			case 0:
+				timer->MCR |= TIMER_MCR_MR0I;
+				break;
+			case 1:
+				timer->MCR |= TIMER_MCR_MR1I;
+				break;
+			case 2:
+				timer->MCR |= TIMER_MCR_MR2I;
+				break;
+			case 3:
+				timer->MCR |= TIMER_MCR_MR3I;
+				break;
+		}
+
+		switch(module)
+		{
+			case 0: 
+				NVIC_EnableIRQ(TIMER0_IRQn);	
+				NVIC_SetPriority(TIMER0_IRQn, 0);
+				break;
+			case 1: NVIC_EnableIRQ(TIMER1_IRQn);	break;
+			case 2: NVIC_EnableIRQ(TIMER2_IRQn);	break;
+			case 3: NVIC_EnableIRQ(TIMER3_IRQn);	break;
+		}
+}
+
+void timer_match_set_value(int module, int channel, int value)
+{
+	LPC_TIM_TypeDef *timer = _modules[module];
+	switch(channel)
+	{
+		case 0:	timer->MR0 = value;	break;
+		case 1:	timer->MR1 = value;	break;
+		case 2:	timer->MR2 = value;	break;
+		case 3: timer->MR3 = value; break;
+	}
+}
+
+void timer_match_set_trigger(int module, int channel, int trigger, int state)
+{
+	LPC_TIM_TypeDef *timer = _modules[module];
+	switch(channel)
+	{
+		case 0:
+			timer->EMR = (timer->EMR & ~(TIMER_EMR_EM0 | TIMER_EMR_EMC0_MASK)) |
+				(trigger << TIMER_EMR_EMC0_BIT) | 
+				(state ? TIMER_EMR_EM0 : 0);
+			break;
+		case 1:
+			timer->EMR = (timer->EMR & ~(TIMER_EMR_EM1 | TIMER_EMR_EMC1_MASK)) |
+				(trigger << TIMER_EMR_EMC1_BIT) | 
+				(state ? TIMER_EMR_EM1 : 0);
+			break;
+		case 2:
+			timer->EMR = (timer->EMR & ~(TIMER_EMR_EM2 | TIMER_EMR_EMC2_MASK)) |
+				(trigger << TIMER_EMR_EMC2_BIT) | 
+				(state ? TIMER_EMR_EM2 : 0);
+			break;
+		case 3:
+			timer->EMR = (timer->EMR & ~(TIMER_EMR_EM3 | TIMER_EMR_EMC3_MASK)) |
+				(trigger << TIMER_EMR_EMC3_BIT) | 
+				(state ? TIMER_EMR_EM3 : 0);
+			break;
+	}	
+}
+
+unsigned long timer_actual(int module)
+{
+	LPC_TIM_TypeDef *timer = _modules[module];
+	return timer->TC;
+}
--- support/lpc17/timer.h
+++ support/lpc17/timer.h
@@ -2,30 +2,55 @@
 #define LPC_TIMER_H
 
 #include "cpu.h"
+#include <support/pwm_hal.h>
 
-typedef enum
-{
-	CAPM_RISING = 1,
-	CAPM_FALLING = 2,
-	CAPM_INT = 4,
-} TIMER_CAPM;
+#define TIMER_IR_MAT0 (1<<0)
+#define TIMER_IR_MAT1 (1<<1)
+#define TIMER_IR_MAT2 (1<<2)
+#define TIMER_IR_MAT3 (1<<3)
+#define TIMER_IR_CAP0 (1<<4)
+#define TIMER_IR_CAP1 (1<<5)
 
-typedef struct
-{
-	TIMER_CAPM CR0:3;
-	TIMER_CAPM CR1:3;
-} TIMER_CCR;
+#define TIMER_MCR_MR0I (1<<0)
+#define TIMER_MCR_MR0R (1<<1)
+#define TIMER_MCR_MR0S (1<<2)
+#define TIMER_MCR_MR1I (1<<3)
+#define TIMER_MCR_MR1R (1<<4)
+#define TIMER_MCR_MR1S (1<<5)
+#define TIMER_MCR_MR2I (1<<6)
+#define TIMER_MCR_MR2R (1<<7)
+#define TIMER_MCR_MR2S (1<<8)
+#define TIMER_MCR_MR3I (1<<9)
+#define TIMER_MCR_MR3R (1<<10)
+#define TIMER_MCR_MR3S (1<<11)
 
-#define TIMER_IR_MR0 (1<<0)
-#define TIMER_IR_MR1 (1<<1)
-#define TIMER_IR_MR2 (1<<2)
-#define TIMER_IR_MR3 (1<<3)
-#define TIMER_IR_CR0 (1<<4)
-#define TIMER_IR_CR1 (1<<5)
+#define TIMER_CCR_CAP0RE	(1<<0)
+#define TIMER_CCR_CAP0FE	(1<<1)
+#define TIMER_CCR_CAP0I		(1<<2)
 
-typedef void (*TIMER_MAT_HANDLER)(LPC_TIM_TypeDef *timer, int channel, unsigned long time);
+#define TIMER_EMR_EM0 (1<<0)
+#define TIMER_EMR_EM1 (1<<1)
+#define TIMER_EMR_EM2 (1<<2)
+#define TIMER_EMR_EM3 (1<<3)
+#define TIMER_EMR_EMC0_BIT (4)
+#define TIMER_EMR_EMC0_MASK (3<<TIMER_EMR_EMC0_BIT)
+#define TIMER_EMR_EMC1_BIT (6)
+#define TIMER_EMR_EMC1_MASK (3<<TIMER_EMR_EMC1_BIT)
+#define TIMER_EMR_EMC2_BIT (8)
+#define TIMER_EMR_EMC2_MASK (3<<TIMER_EMR_EMC2_BIT)
+#define TIMER_EMR_EMC3_BIT (10)
+#define TIMER_EMR_EMC3_MASK (3<<TIMER_EMR_EMC3_BIT)
 
-int timer_initialize(int module, unsigned long freq, TIMER_MAT_HANDLER callback);
+#define TIMER_EMC_NOTHING 0
+#define TIMER_EMC_CLEAR 1
+#define TIMER_EMC_SET 2
+#define TIMER_EMC_TOGGLE 3
+
+int timer_match_initialize(int module, int range, int rate, int channel_for_period);
+void timer_match_set_handler(int module, int channel, HAL_PWM_HANDLER callback);
+void timer_match_set_value(int module, int channel, int value);
+void timer_match_set_trigger(int module, int channel, int trigger, int state);
+unsigned long timer_actual(int module);
 
 #endif // LPC_TIMER_H
 
