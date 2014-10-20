4dfe0874f02dcf8f3ebb3eb73689d2b1ac6411f3
Miguel Fides <miguelfides@gmail.com>
2014-10-20 16:41:24 +0200

    preliminary mioboard2 support

--- support/board/board_mioboard_lpc1778.c
+++ support/board/board_mioboard_lpc1778.c
@@ -36,6 +36,41 @@ void hal_board_initialize()
 	#define LED1_MASK (1<<23)
 	LED1_PORT->CLR = LED1_MASK;
 
+#elif defined BOARD_MIOBOARD2
+
+	pincon_setfunc(0, 27, 1);	// SDA0
+	pincon_setfunc(0, 28, 1);	// SCL0
+	pincon_setfunc(0, 19, 3);	// SDA1
+	pincon_setfunc(0, 20, 3);	// SCL1
+	pincon_setfunc(0, 10, 2);	// SDA2
+	pincon_setfunc(0, 11, 2);	// SCL2
+	pincon_setmode(0, 10, IOCON_MODE_PULL_UP, PINCONF_HYS | PINCONF_OPEN_DRAIN);
+	pincon_setmode(0, 11, IOCON_MODE_PULL_UP, PINCONF_HYS | PINCONF_OPEN_DRAIN);
+
+	// usb1
+	pincon_setfunc(0, 29, 1);	// D+
+	pincon_setfunc(0, 30, 1);	// D-
+	pincon_setfunc(1, 18, 1);	// USB_UP_LED1
+	pincon_setfunc(1, 19, 2);	// _USB_PPWR1
+	pincon_setfunc(1, 22, 2);	// USB_PWRD1
+	pincon_setfunc(1, 27, 2);	// _USB_OVRCR1
+	
+	// usb2
+	pincon_setfunc(0, 31, 1);	// D+
+	pincon_setfunc(0, 13, 1);	// USB_UP_LED2
+	pincon_setfunc(0, 12, 1);	// _USB_PPWR2
+	pincon_setfunc(1, 30, 1);	// USB_PWRD2
+	pincon_setfunc(1, 31, 1);	// _USB_OVRCR2
+	
+//	pincon_setfunc(0, 0, 1);	// CAN_RD1
+//	pincon_setfunc(0, 1, 1);	// CAN_TD1 
+//	pincon_setfunc(2, 7, 1);	// CAN_RD2
+//	pincon_setfunc(2, 8, 1);	// CAN_TD2 
+
+	#define LED1_PORT LPC_GPIO3
+	#define LED1_MASK (1<<23)
+	LED1_PORT->CLR = LED1_MASK;
+
 #elif defined BOARD_UMFI
 
 	pincon_setfunc(0, 27, 1);	// SDA0
