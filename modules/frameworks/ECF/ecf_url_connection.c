6239e200f3acd19610c8191494137d0c036d843f
Javier Segura <javier.segura@me.com>
2014-11-25 19:51:06 +0100

    added cf_url_connection to support http GET and POST connections

---
+++ modules/frameworks/ECF/ecf_url_connection.c
@@ -0,0 +1,60 @@
+//
+//  ecf_url_connection.c
+//  ExOS Core Foundation
+//
+//  Created by GodShadow on 25/11/14.
+//  Copyright (c) 2014 MIO Research Labs. All rights reserved.
+//
+
+#include "ecf_url_connection.h"
+#include <stdlib.h>
+
+struct ecf_url_connection_struct
+{
+    char *url_string;
+    
+};
+
+ecf_url_connection *ecf_url_connection_create(char *url_string)
+{
+    if (url_string == NULL)
+        return NULL;
+    
+    ecf_url_connection *conn = malloc(sizeof(struct ecf_url_connection_struct));
+    if (conn == NULL)
+        return NULL;
+
+    conn->url_string = url_string;
+    
+    return conn;
+}
+
+void ecf_url_connection_destroy(ecf_url_connection *url_connection)
+{
+    
+}
+
+void ecf_url_connection_set_http_method(ecf_url_connection *url_connection, char *http_method)
+{
+    
+}
+
+void ecf_url_connection_add_http_header_value(ecf_url_connection *url_connection, char *header, char *value)
+{
+    
+}
+
+void ecf_url_connection_set_http_body(ecf_url_connection *url_connection, ecf_data *data)
+{
+    
+}
+
+void ecf_url_connection_start(ecf_url_connection *url_connection)
+{
+    
+}
+
+void ecf_url_connection_stop(ecf_url_connection *url_connection)
+{
+    
+}
---
+++ modules/frameworks/ECF/ecf_url_connection.h
@@ -0,0 +1,27 @@
+//
+//  ecf_url_connection.h
+//  ExOS Core Foundation
+//
+//  Created by GodShadow on 25/11/14.
+//  Copyright (c) 2014 MIO Research Labs. All rights reserved.
+//
+
+#ifndef __ECF_URL_CONNECTION__
+#define __ECF_URL_CONNECTION__
+
+#include "ecf_types.h"
+#include "ecf_data.h"
+
+typedef struct ecf_url_connection_struct ecf_url_connection;
+
+ecf_url_connection *ecf_url_connection_create(char *url_string);
+void ecf_url_connection_destroy(ecf_url_connection *url_connection);
+
+void ecf_url_connection_set_http_method(ecf_url_connection *url_connection, char *http_method);
+void ecf_url_connection_add_http_header_value(ecf_url_connection *url_connection, char *header, char *value);
+void ecf_url_connection_set_http_body(ecf_url_connection *url_connection, ecf_data *data);
+
+void ecf_url_connection_start(ecf_url_connection *url_connection);
+void ecf_url_connection_stop(ecf_url_connection *url_connection);
+
+#endif /* defined(__ECF_URL_CONNECTION__) */
