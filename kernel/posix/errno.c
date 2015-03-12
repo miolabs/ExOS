a7d5c4bfba13eb9eb520634eba829333dd51fa4f
Miguel Fides <miguelfides@gmail.com>
2015-03-12 22:14:01 +0100

    posix errno for crossworks lib compatibility

--- kernel/posix/errno.c
+++ kernel/posix/errno.c
@@ -1,3 +1,16 @@
 //#include <errno.h>
 
 int __errno;
+
+#ifdef __ARM_EABI__
+
+const int __aeabi_EDOM = 1;
+const int __aeabi_EILSEQ = 2;
+const int __aeabi_ERANGE = 3;
+
+volatile int *__aeabi_errno_addr(void)
+{
+	return &__errno;
+}
+
+#endif
