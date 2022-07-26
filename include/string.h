21c3d00c9393d15109e530c4aafad491cfe07ed9
Miguel Fides <miguelfides@gmail.com>
2022-07-26 18:56:45 +0200

    more string functions

--- include/string.h
+++ include/string.h
@@ -7,9 +7,11 @@ void *memset(void *s, int c, size_t n);
 
 void *memcpy(void *restrict s1, const void *restrict s2, size_t n);
 
-int strcmp(const char *, const char *);
-size_t strlen(const char * str);
+int strcmp(const char *str1, const char *str2);
+size_t strlen(const char *str);
 char *strcpy(char *dest, const char *src);
+int strncmp(const char *str1, const char *str2, size_t num);
+char *strncpy(char *dest, const char *src, size_t num);
 
 char *strerror(int errnum);
 
--- kernel/posix/string.c
+++ kernel/posix/string.c
@@ -23,7 +23,7 @@ int strcmp(const char *s1, const char *s2)
 	return __str_comp(s1, s2);
 }
 
-size_t strlen(const char * str)
+size_t strlen(const char *str)
 {
 	int i=0;
 	while(str[i])
@@ -37,4 +37,37 @@ char *strcpy(char *dest, const char *src)
 	return dest;
 }
 
+int strncmp(const char *str1, const char *str2, size_t num)
+{
+	for (size_t index = 0; index < num; index++)
+	{
+		char c1 = str1[index];
+		char c2 = str2[index];
+		if (c1 == c2)
+		{
+			if (c1 == 0)
+				break;
+			continue;
+		}
+		else return c1 < c2 ? -1 : 1;
+	}
+	return 0;
+}
+
+char *strncpy(char *dest, const char *src, size_t num)
+{
+	unsigned length = 0;
+	while(length < num)
+	{
+		char c = src[length];
+		dest[length++] = c;
+		if (c == '\0')
+		{
+			while(length < num)
+				dest[length++] = '\0';
+			break;
+		}
+	}
+	return dest;
+}
 
