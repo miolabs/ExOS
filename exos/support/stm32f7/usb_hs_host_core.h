55732a9790e7affe38bdb3eee8af0fc962e7def9
Miguel Fides <miguelfides@gmail.com>
2022-10-11 21:59:05 +0200

    stm32f7 host with ulpi phy support

---
+++ exos/support/stm32f7/usb_hs_host_core.h
@@ -0,0 +1,61 @@
+#ifndef STM32F7_USB_OTG_HS_HOST_CORE_H
+#define STM32F7_USB_OTG_HS_HOST_CORE_H
+
+#include <usb/host.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <kernel/dispatch.h>
+#include <kernel/event.h>
+
+typedef enum
+{
+	PID_DATA0 = 0,
+	PID_DATA2, PID_DATA1, PID_SETUP,
+} stm32_usbh_pid_t;
+
+typedef struct
+{
+	usb_request_buffer_t *Request;
+	unsigned char LastPacketLength;
+} stm32_usbh_xfer_t;
+
+typedef struct
+{
+	uint8_t Index;
+	uint8_t EndpointNumber;
+	usb_transfer_type_t EndpointType;
+    usb_direction_t Direction;
+	stm32_usbh_pid_t Pid;
+	unsigned char Toggle;
+	unsigned char Period;
+	stm32_usbh_xfer_t Current;
+} stm32_usbh_channel_t;
+
+typedef enum
+{
+	STM32_EP_STA_IDLE = 0,
+    STM32_EP_STA_BUSY,
+	STM32_EP_STA_STOPPING,
+} stm32_usbh_ep_state_t;
+
+typedef struct
+{
+	node_t Node;	// for pooling
+	stm32_usbh_channel_t *Tx;
+	stm32_usbh_channel_t *Rx;
+	stm32_usbh_ep_state_t Status;
+} stm32_usbh_ep_t;
+ 
+void usb_hs_host_initialize(usb_host_controller_t *hc, dispatcher_context_t *context);
+void usb_hs_host_port_reset();
+bool usb_hs_host_start_pipe(usb_host_pipe_t *pipe);
+void usb_hs_host_update_control_pipe(usb_host_pipe_t *pipe);
+void usb_hs_host_stop_pipe(usb_host_pipe_t *pipe);
+
+void usb_hs_host_flush_tx_fifo(unsigned num);	// FIXME
+void usb_hs_host_flush_rx_fifo();	// FIXME
+
+bool usb_hs_host_begin_xfer(usb_request_buffer_t *urb, usb_direction_t dir, bool setup);
+
+#endif // STM32F7_USB_OTG_HS_HOST_CORE_H
+
---
+++ exos/support/stm32f7/usb_otg_hs.c
@@ -0,0 +1,110 @@
+#include "usb_otg_hs.h"
+#include "cpu.h"
+#include <kernel/verbose.h>
+#include <kernel/panic.h>
+#include <string.h>
+
+static usb_otg_crs_global_t * const otg_global = (usb_otg_crs_global_t *)(USB_OTG_HS_BASE + 0x000);
+static usb_otg_crs_power_t * const otg_power = (usb_otg_crs_power_t *)(USB_OTG_HS_BASE + 0xe00);
+
+#define STM32_USB_HS_ULPI	// FIXME: temporary
+
+void usb_otg_hs_initialize()
+{
+	// core initialization
+	RCC->AHB1RSTR |= RCC_AHB1RSTR_OTGHRST;
+	RCC->AHB1ENR |= RCC_AHB1ENR_OTGHSEN;	// enable peripheral clock
+#ifdef STM32_USB_HS_ULPI
+	RCC->AHB1ENR |= RCC_AHB1ENR_OTGHSULPIEN;	// enable ulpi clock
+#endif
+	RCC->AHB1RSTR ^= RCC_AHB1RSTR_OTGHRST;
+
+#ifdef STM32_USB_HS_ULPI
+	otg_global->GUSBCFG |= (1<<4); // FIXME: undefined USB_OTG_GUSBCFG_ULPISEL
+	otg_global->GUSBCFG |= USB_OTG_GUSBCFG_ULPIEVBUSD | USB_OTG_GUSBCFG_ULPIIPD;
+#else
+	otg_global->GUSBCFG |= USB_OTG_GUSBCFG_PHYSEL;
+#endif
+
+	while(!(otg_global->GRSTCTL & USB_OTG_GRSTCTL_AHBIDL));
+	for(unsigned volatile i = 0; i < 100; i++);	// wait 3 phy clks
+
+
+	otg_global->GRSTCTL |= USB_OTG_GRSTCTL_CSRST;	// soft-reset
+	while(otg_global->GRSTCTL & USB_OTG_GRSTCTL_CSRST);	// NOTE: self-clearing
+	for(unsigned volatile i = 0; i < 100; i++);	// wait 3 phy clks
+
+	// enable PHY
+	otg_global->GCCFG = USB_OTG_GCCFG_PWRDWN/* | USB_OTG_GCCFG_NOVBUSSENS*/;	// <--- FIXME
+	exos_thread_sleep(20);
+
+#if defined(STM32_USB_HS_ULPI) && defined(DEBUG)
+	unsigned char ulpi_id[4];
+	for(unsigned i = 0; i < sizeof(ulpi_id); i++) 
+	{
+		if (!usb_otg_hs_ulpi_read(i, &ulpi_id[i]))
+		{
+			verbose(VERBOSE_ERROR, "otg-hs", "ulpi-phy read failed!");
+			break;
+		}
+	}
+	verbose(VERBOSE_DEBUG, "otg-hs", "ulpi phy vendor:id=%02x%02x:%02x%02x", 
+		ulpi_id[1], ulpi_id[0], ulpi_id[3], ulpi_id[2]);
+#endif
+
+	otg_global->GINTMSK = USB_OTG_GINTMSK_MMISM;	// Mode MISmatch Mask
+
+	otg_global->GAHBCFG |= OTG_HS_GAHBCFG_GINTMASK;
+	NVIC_EnableIRQ(OTG_HS_IRQn);
+}
+
+bool usb_otg_hs_ulpi_read(unsigned short addr, unsigned char *pdata)
+{
+	bool done = false;
+	otg_global->PHYCR = OTG_HS_PHYCR_NEW | (addr << 16);
+	for (unsigned timeout = 100; timeout != 0; timeout--)
+	{
+		unsigned val = otg_global->PHYCR;
+		if (val & OTG_HS_PHYCR_DONE)
+		{
+			*pdata = val & OTG_HS_PHYCR_DATA;
+			done = true;
+			break;
+		}
+	}
+	return done;
+}
+
+bool usb_otg_hs_ulpi_write(unsigned short addr, unsigned char data)
+{
+	bool done = false;
+	otg_global->PHYCR = OTG_HS_PHYCR_NEW | OTG_HS_PHYCR_RW | (addr << 16) | data;
+	for (unsigned timeout = 10; timeout != 0; timeout--)
+	{
+		unsigned val = otg_global->PHYCR;
+		if (val & OTG_HS_PHYCR_DONE)
+		{
+			done = true;
+			break;
+		}
+	}
+	return done;
+}
+
+
+
+
+
+
+void OTG_HS_IRQHandler()
+{
+	if (otg_global->GINTSTS & USB_OTG_GINTSTS_MMIS)
+	{
+		otg_global->GINTSTS = USB_OTG_GINTSTS_MMIS; // clear int
+	}
+
+	__usb_otg_hs_host_irq_handler();
+    __usb_otg_hs_device_irq_handler();
+}
+
+
