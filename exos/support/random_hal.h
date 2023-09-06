8c95667ce2b42e96c746d0b328eb969ba815d0ef
Miguel Fides <miguelfides@gmail.com>
2023-09-06 20:45:42 +0200

    simplified random hal

--- exos/support/random_hal.h
+++ exos/support/random_hal.h
@@ -6,10 +6,30 @@ typedef struct
     unsigned int Seed;
 } hal_random_context_t;
 
-void hal_random_context_create(hal_random_context_t *context, unsigned seed);
-int hal_random_read(hal_random_context_t *context, unsigned char *buffer, unsigned length);
 
 int hal_random_hw(hal_random_context_t *context, unsigned char *buffer, unsigned length);
+
+
+static void hal_random_context_create(hal_random_context_t *context, unsigned seed)
+{
+	*context = (hal_random_context_t) { .Seed = seed };
+}
+
+static int hal_random_read(hal_random_context_t *context, unsigned char *buffer, unsigned length)
+{
+	int done = 0;
+	while(done < length)
+	{
+		int chunk = hal_random_hw(context, buffer + done, length - done);
+		if (chunk == 0)
+			return -1;
+		
+		done += chunk;
+		length -= chunk;
+	}
+	return done;
+}
+
 
 #endif // HAL_RANDOM_HAL_H
 
